26e1b4a2c9715d971d76bb7550cab5ae92500780
sepideh2001 <100096609+sepideh2001@users.noreply.github.com>
2022-03-13 18:37:54 -0700

    Add files via upload

---
+++ py.t 41.py
@@ -0,0 +1,16 @@
+max_value = int(input('Display primes up to what value? '))
+value = 2 
+while value <= max_value:
+  is_prime = True 
+  trial_factor = 2
+  while trial_factor < value:
+       if value % trial_factor == 0:
+          is_prime = False 
+          break
+       trial_factor += 1
+  if is_prime:
+       print(value, end='') 
+  value += 1
+print() 
+
+#Display prices up to what value? 90 2357317 19 23 29-3174113473 59 61 67713 79 83 89
