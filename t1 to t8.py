0b6124b1aa4d01f83404c8ccecf99454c03eaf7c
sepideh2001 <100096609+sepideh2001@users.noreply.github.com>
2022-05-08 15:31:24 -0700

    Add files via upload

---
+++ t1 to t8.py
@@ -0,0 +1,49 @@
+t.1:
+yes
+
+t.2:
+it will give you the numbers of the list from the end
+
+t.3:
+a = [45, -3, 16, 8]
+
+t.4:
+(a)lst[0]
+(b)lst[3]
+(c)10
+(d)29
+(e)-4
+(f)29
+(g)10
+(h)illigal
+
+t.5:
+a 3
+b 5
+c 1
+d 5
+e 5
+f 2
+g 0
+h 3
+
+t.6:
+function len
+
+t.7:
+a=[]
+
+t.8:
+a [20, 1, -34, 40, -8, 60, 1, 3]
+b [20,1,-34]
+c [-8,60,1,3]
+d [-8,60,1,3]
+e [40,-8]
+f [20,1,-34]
+g [-8,60,1,3]
+h [20,1,-34,40]
+i [20,1,-34,40]
+j [1,-34,40,-8]
+k true
+l false
+m 8
