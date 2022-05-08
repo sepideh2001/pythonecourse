d68bca14f99b178813ee694674f6c66545bf4be4
sepideh2001 <100096609+sepideh2001@users.noreply.github.com>
2022-05-08 15:32:17 -0700

    Add files via upload

---
+++ t10 to t14.py
@@ -0,0 +1,25 @@
+t10:
+a [8,8,8,8]
+b [2,7,2,7,2,7,2,7,2,7,2,7]
+c [1,2,3,'a','b','c','d']
+d [1,2,1,2,1,2,4,2]
+e [1,2,4,2,1,2,4,2,1,2,4,2]
+
+t11:
+a [3,5,7,9]
+b [50,60,70,80,90]
+c [12,15,18]
+d [(0,0),(0,1),(0,2),(0,3),(1,0),(1,1),(1,2),(1,3),(2,0),(2,1),(2,2),(2,3)]
+e [(0,0),(0,2),(1,1),(1,3),(2,0),(2,2)]
+
+t12:
+(a) [x**2 for x in range (1,6)]
+(b) [x/4 for x in range (1,7)]
+(c) [(x,y) for x in ['B','b'] for y in [0,1,2]]
+
+t13:
+x in lst (show existence)
+x not in lst (show not existence)
+
+t14:
+reverse()function is used to reverse the order of objects in a list data structure in place.
