1f0d0b2a97a2fefd0926b5c886a5e296abe326a0
sepideh2001 <100096609+sepideh2001@users.noreply.github.com>
2022-05-08 15:41:06 -0700

    Update t16.py

--- t16.py
+++ t16.py
@@ -3,6 +3,6 @@ def count_evens(lst):
     evens_num = 0
     for i in lst:
         if i % 2 ==0:
-            events_num = evens_num + 1
-    print(events_num)
+            evens_num = evens_num + 1
+    print(evens_num)
 count_evens(lst)
